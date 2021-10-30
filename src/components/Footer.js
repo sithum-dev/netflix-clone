f1fff82d667cb0579a62d60e6819dd681edf7db2
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2021-10-30 14:34:45 +0530

    user icon update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import TMDBLogo from '../static/images/tmdb.svg'
 
 const footer = () => (
   <footer className='footer'>
+    <TMDBLogo className='footer__tmdb-logo' />
     <div className='footer__copyright'>
       &copy; 2021 Made with ❤️ by{' '}
       <a className='footer__copyright--link' href='www.sithumimarsha.com'>
