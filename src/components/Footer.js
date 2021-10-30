494c8b4ed439603e170367457b5ca78ace3ee4ef
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2021-10-30 09:03:37 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import TMDBLogo from '../static/images/tmdb.svg'
+
+const footer = () => (
+  <footer className='footer'>
+    <div className='footer__copyright'>
+      &copy; 2021 Made with ❤️ by{' '}
+      <a className='footer__copyright--link' href='www.sithumimarsha.com'>
+        {' '}
+        Sithum Imarsha
+      </a>
+    </div>
+  </footer>
+)
+
+export default footer
